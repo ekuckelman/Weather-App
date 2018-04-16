89280c425f9e515d2e8a6fe3e8f6002548369cae
Emily Kuckelman <ekuckelman@gmail.com>
2018-04-16 16:18:26 -0600

    App tests completed

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -20,9 +20,23 @@ describe ('App', () => {
     });
  })
 
+ it('should render only the welcome screen and search bar if there is no clean data', ()=> {
+  wrapper.setState({cleanData: false });
+  expect(wrapper.find('Welcome').length).toEqual(1);
+  expect(wrapper.find('Search').length).toEqual(1);
+ })
+
  it('should render the error message is the weather cannot be found', () => {
-  wrapper.setState({fetchError: true});
+  wrapper.setState({fetchError: true });
   expect(wrapper.find('h2.error').text()).toEqual(' Please search for an Actual location (ex: Kechi, KS OR 67067)');
  });
 
+ it('should render the current weather, seven hour, and ten day components if there is data and an error', () => {
+  wrapper.setState({fetchError: true });
+  wrapper.setState({cleanData: true });
+
+  expect(wrapper.find('CurrentWeather').length).toEqual(1);
+  expect(wrapper.find('SevenHour').length).toEqual(1);
+  expect(wrapper.find('TenDay').length).toEqual(1);
+ });
 })
